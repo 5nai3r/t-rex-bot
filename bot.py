7d2a809d8ac7aa9a3ffdf8b981e09c02ee63c6b8
unknown <dzmakr@gmail.com>
2020-04-07 19:06:39 +0100

    Added GUI to show detection zone and buttons for updating the zone and enabling the bot

--- bot.py
+++ bot.py
@@ -1,15 +1,54 @@
 import pyautogui
 import time
+from tkinter import *
+import threading
 #project code dala3
 #zone where take to screenshot to detect the cactus  
-detectionZone = (530,150, 20, 200) 
+detectionZone = (476,176, 20, 200) 
+#enable variable for the loop
+BotEnable = False 
+exitLoop = False
 
-while True:
-	s = pyautogui.screenshot(region=detectionZone)
-	signature = sum(list( s.convert('1').getdata()))
-	if signature < 960940 :
-		print('jump')
-		print(signature)
-		pyautogui.keyDown("space")
-		time.sleep(0.3)
-		pyautogui.keyUp("space")
+def checkLoop():
+	while not exitLoop:
+		if BotEnable :
+			s = pyautogui.screenshot(region=detectionZone)
+			signature = sum(list( s.convert('1').getdata()))
+			if signature < 960940 :
+				print('jump')
+				print(signature)
+				pyautogui.keyDown("space")
+				time.sleep(0.3)
+				pyautogui.keyUp("space")
+	return
+
+x = threading.Thread(target=checkLoop)
+x.start()
+	
+BotWindow = Tk()
+BotWindow.wm_attributes('-alpha',0.2)
+
+w = Canvas(BotWindow, width=200, height=250)
+w.pack()
+w.create_rectangle(50, 25, 20, 200, fill="blue")
+def click():
+    global detectionZone
+    detectionZone = (BotWindow.winfo_x()+50, BotWindow.winfo_y()+25,20,200)
+
+def toggleEnable():
+    global BotEnable
+    BotEnable = not BotEnable
+    enbBtn['bg']="green" if BotEnable else "red"
+
+def onClose():
+	global exitLoop
+	BotWindow.destroy()
+	exitLoop = True
+	
+posBtn = Button(BotWindow,text="Update position", command=click)
+enbBtn = Button(BotWindow, text='Toggle',command=toggleEnable)
+posBtn.pack()
+enbBtn.pack()
+
+BotWindow.protocol("WM_DELETE_WINDOW", onClose)
+BotWindow.mainloop()
