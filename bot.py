0fdaab49b3f05a709b6dec094abc73edc149e37c
unknown <dzmakr@gmail.com>
2020-04-07 14:21:09 +0100

    adding main screen capture and jumping loop TODO: -add visual hint of where is the capture region is -make longer "space" press, since the T-rex jump higher by  long press

--- bot.py
+++ bot.py
@@ -1,2 +1,13 @@
 import pyautogui
 import time
+#project code dala3
+#zone where take to screenshot to detect the cactus  
+detectionZone = (530,150, 20, 200) 
+
+while True:
+	s = pyautogui.screenshot(region=detectionZone)
+	signature = sum(list( s.convert('1').getdata()))
+	if signature < 960940 :
+		print('jump')
+		print(signature)
+		pyautogui.press('space')
